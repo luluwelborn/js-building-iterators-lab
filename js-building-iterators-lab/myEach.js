a296f4e4c03cbe536e30e6985f41f460bdea3445
luluwelborn <lulu.welborn@gmail.com>
2017-07-05 09:06:25 -0600

    Add files via upload

---
+++ js-building-iterators-lab/myEach.js
@@ -0,0 +1,23 @@
+// See https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/ForEach
+
+function myEach(arr, callback) {
+  
+  //  CODE INSIDE HERE   //
+  for (var i=0; i < arr.length; i++){
+  	callback(arr[i],i,arr);
+  }
+}
+
+/*
+ Best if you don't code out here.
+ If you want to check your code, use `index.js`!
+*/
+
+
+
+
+
+
+
+// export this function (you can ignore this for now)
+module.exports = myEach;
---
+++ js-building-iterators-lab/myMap.js
@@ -0,0 +1,21 @@
+// See https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/Map
+
+function myMap(arr, callback) {
+
+//  CODE INSIDE HERE   //
+
+}
+
+/*
+ Best if you don't code out here.
+ If you want to check your code, use `index.js`!
+*/
+
+
+
+
+
+
+
+// export this function (you can ignore this for now)
+module.exports = myMap;
